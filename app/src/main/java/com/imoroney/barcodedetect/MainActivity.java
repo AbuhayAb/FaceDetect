a69900ce18eb2da4589473ea93659d4e1debcd08
abuhay.abune <T2c5k_l8sB>
2018-10-17 20:06:48 +0300

    create function: detectFace, createFaceDetector, createCanvasObject

--- app/src/main/java/com/imoroney/barcodedetect/MainActivity.java
+++ app/src/main/java/com/imoroney/barcodedetect/MainActivity.java
@@ -2,19 +2,32 @@ package com.imoroney.barcodedetect;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
     private ImageView myImageView;
     private Paint myRectPaint;
+    private Bitmap myBitmap;
+    private FaceDetector faceDetector;
+    private Frame frame ;
+    private SparseArray<Face> faces ;
+    private String msg_err="Could not set up the face detector!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,25 +38,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 loadTheImage();
                 createPaintObject();
+                createCanvasObject(myBitmap);
+                createFaceDetector(v);
             }
         });
     }
-    public void loadTheImage()
-    {
+
+    public void loadTheImage() {
         myImageView = (ImageView) findViewById(R.id.imgview);
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable=true;
-        Bitmap myBitmap = BitmapFactory.decodeResource(
+        options.inMutable = true;
+        myBitmap = BitmapFactory.decodeResource(
                 getApplicationContext().getResources(),
                 R.drawable.test1,
                 options);
 
     }
-    public void createPaintObject()
-    {
+
+    public void createPaintObject() {
         myRectPaint = new Paint();
         myRectPaint.setStrokeWidth(5);
         myRectPaint.setColor(Color.RED);
         myRectPaint.setStyle(Paint.Style.STROKE);
     }
+
+    public void createCanvasObject(Bitmap myBitmap) {
+        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(),
+                                                myBitmap.getHeight(),
+                                                Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+    }
+
+    public void createFaceDetector(View v) {
+        faceDetector = new FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                .build();
+        if (!faceDetector.isOperational()) {
+            new AlertDialog.Builder(v.getContext()).setMessage(msg_err).show();
+            return;
+        }
+    }
+    public void detectFace() {
+        frame = new Frame.Builder().setBitmap(myBitmap).build();
+        faces = faceDetector.detect(frame);
+    }
+
 }
